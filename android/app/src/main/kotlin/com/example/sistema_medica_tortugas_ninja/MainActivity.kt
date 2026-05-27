d6cdaa830e85efdb4d89caf20aa03885b0980a40
CrizJimz <c.chrisjim@gmail.com>
2026-05-27 10:55:11 -0600

    Revert "Merge branch 'main' of https://github.com/AlanArrietaF/sistema_medica_tortugas_ninja"
    This reverts commit a13000d5c8181704d82405a34679f584b20d0057, reversing
    changes made to a59f1e20db45f46017d0fd6c8c765b0e51fca7b1.

--- android/app/src/main/kotlin/com/example/sistema_medica_tortugas_ninja/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sistema_medica_tortugas_ninja
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'home.dart'; // Esto llama a tu archivo de las tortugas ninja
-
-void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
-  ));
-}
